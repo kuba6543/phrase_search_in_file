8c862764e94b5e1544c83c84779d31170539990d
kuba6543 <berta12389@wp.pl>
2022-11-05 17:30:41 +0100

    Working, but doesn't show where the phrases are

--- Bartuszek_Jakub_318490.c
+++ Bartuszek_Jakub_318490.c
@@ -1,26 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void read_variables(char path[255],char string[255])
+void open_file(char path[255])
 {
-	printf("Insert path to your file:\n");
-	scanf("%s",path);
-	printf("Insert searched string:\n");
-	scanf("%s",string);
+	FILE * file = fopen(path,"r");
+	if(file)
+	{
+		printf("File has been opened!\n");	
+	}
+	else
+	{
+		printf("Couldn't open a file, exiting...\n");
+		exit(-1);
+	}
 }
 
-void look_for_variables(char path[255],char string[255])
+void look_for_phrase(char path[255],char phrase[255])
 {
+	char temp[255] = "";
+	int quantity = 0;
 
+	FILE * file = fopen(path,"r");
+    while(fscanf(file, "%s", &temp)!=EOF)
+    {
+		if(!stricmp(phrase, temp))
+		{
+			printf("Phrase \"%s\" found!\n",phrase);
+			quantity++;
+		}
+	}
+    fclose(file);
+    printf("Number of phrases \"%s\" in file: %d\n", phrase, quantity);
+	
 }
-
 void main()
 {
-	char searched_string[255] = "";
+	char searched_phrase[255] = "";
 	char path_to_file[255] = "";
-	int quantity = 0;
-	read_variables(path_to_file, searched_string);
-	look_for_variables(path_to_file, searched_string);
+	printf("Insert path to your file:\n");
+	scanf("%s",path_to_file);
+	printf("Insert searched phrase:\n");
+	scanf("%s",searched_phrase);
 
-
+	open_file(path_to_file);
+	look_for_phrase(path_to_file, searched_phrase);
 }
