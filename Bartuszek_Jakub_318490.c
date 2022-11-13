91fa6b54bc2a28a60b7b1384b5993caf3befb01d
kuba6543 <berta12389@wp.pl>
2022-11-13 20:47:29 +0100

    More subtle changes

--- Bartuszek_Jakub_318490.c
+++ Bartuszek_Jakub_318490.c
@@ -2,26 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-void check_file(char path[255])
+void look_for_phrase(char path[255],char phrase[255])
 {
-	FILE * file = fopen(path,"r");
-	if(file)
+	char temp[255] = ""; 								// temporary function to store lines in file 
+	int quantity = 0;									// quantity of occurences
+
+	FILE * file = fopen(path,"r"); 						// opening file from path
+	if(file) 											// checking if the file is open, file not opened will return NULL (0)
 	{
 		printf("File has been opened!\n");	
 	}
 	else
 	{
 		printf("Couldn't open a file, exiting...\n");
-		exit(-1);
+		exit(-1);										// exiting the program with error in case of not opening the file
 	}
-}
-
-void look_for_phrase(char path[255],char phrase[255])
-{
-	char temp[255] = "";
-	int quantity = 0;
-
-	FILE * file = fopen(path,"r");
     while(fscanf(file, "%s", &temp)!=EOF)
     {
 		if(!strcmp(phrase, temp))
@@ -30,7 +25,7 @@ void look_for_phrase(char path[255],char phrase[255])
 			quantity++;
 		}
 	}
-    fclose(file);
+    fclose(file);										// closing the file
     printf("Number of phrases \"%s\" in file: %d\n", phrase, quantity);
 	
 }
@@ -39,15 +34,14 @@ void main(int argc, char *argv[])
 	char searched_phrase[255] = "";
 	char path_to_file[255] = "";
 
-	if (argc!=3)
+	if (argc!=3)																// 2 arguments expected +  program = 3
 	{
 		printf("Wrong input, use ./<filename> <searched_phrase> <path_to_file>");
 		exit(-1);
 	}
 
-    strcpy(searched_phrase, argv[1]);
+    strcpy(searched_phrase, argv[1]);											// strings can't be simply copied, I am using strcpy()
     strcpy(path_to_file, argv[2]);
 
-	check_file(path_to_file);
 	look_for_phrase(path_to_file, searched_phrase);
 }
